97ef8843243f4b16827ad94001c6400cdab0d95f
tanvi4248 <tanvi@GWTN141-10>
2024-04-10 11:47:43 -0500

    create tables for timing

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,12 +1,28 @@
 const client = require("./client");
-const { sliders } = require("./seedData");
+const {
+  sliders,
+  dtimings,
+  atimings,
+  ttimings,
+  stimings,
+} = require("./seedData");
 const { createSlider } = require("./sqlHelperFunctions/sliders");
+const {
+  createTiming,
+  createAtiming,
+  createTtiming,
+  createStiming,
+} = require("./sqlHelperFunctions/timing");
 
 const dropTables = async () => {
   try {
     console.log("Starting to drop tables...");
     await client.query(`
       DROP TABLE IF EXISTS sliders CASCADE;
+      DROP TABLE IF EXISTS dtimings CASCADE;
+      DROP TABLE IF EXISTS atimings CASCADE;
+      DROP TABLE IF EXISTS ttimings CASCADE;
+      DROP TABLE IF EXISTS stimings CASCADE;
       `);
     console.log("Table Dropped!");
   } catch (error) {
@@ -21,6 +37,26 @@ const createTable = async () => {
       CREATE TABLE sliders(
         id SERIAL PRIMARY KEY,
         image TEXT NOT NULL
+      );
+      CREATE TABLE dtimings(
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        day TEXT NOT NULL
+      );
+      CREATE TABLE atimings(
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        day TEXT NOT NULL
+      );
+      CREATE TABLE ttimings(
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        day TEXT NOT NULL
+      );
+      CREATE TABLE stimings(
+        id SERIAL PRIMARY KEY,
+        title TEXT NOT NULL,
+        day TEXT NOT NULL
       );
       `);
     console.log("Tables Created!");
@@ -40,12 +76,57 @@ const createInitialSlider = async () => {
   }
 };
 
+const createInitialTimings = async () => {
+  try {
+    for (const dtiming of dtimings) {
+      await createTiming(dtiming);
+    }
+    console.log("created dtiming");
+  } catch (error) {
+    throw error;
+  }
+};
+
+const createInitialAtimings = async () => {
+  try {
+    for (const atiming of atimings) {
+      await createAtiming(atiming);
+    }
+    console.log("created atiming");
+  } catch (error) {
+    throw error;
+  }
+};
+const createInitialTtimings = async () => {
+  try {
+    for (const ttiming of ttimings) {
+      await createTtiming(ttiming);
+    }
+    console.log("created ttiming");
+  } catch (error) {
+    throw error;
+  }
+};
+const createInitialStimings = async () => {
+  try {
+    for (const stiming of stimings) {
+      await createStiming(stiming);
+    }
+    console.log("created stiming");
+  } catch (error) {
+    throw error;
+  }
+};
 const buildDb = async () => {
   try {
     client.connect();
     await dropTables();
     await createTable();
     await createInitialSlider();
+    await createInitialTimings();
+    await createInitialAtimings();
+    await createInitialTtimings();
+    await createInitialStimings();
   } catch (error) {
     console.error(error);
   } finally {
