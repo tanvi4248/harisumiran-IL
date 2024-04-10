71d17c1c131209adbf0eda27e2446d65a3105955
tanvi4248 <tanvi@GWTN141-10>
2024-04-10 11:48:31 -0500

    add sliders image

--- server/db/seedData.js
+++ server/db/seedData.js
@@ -1,16 +1,62 @@
 const sliders = [
   {
-    image:
-      "https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    image: "../src/assets/slider/slider-01.jpg",
   },
   {
-    image:
-      "https://images.unsplash.com/photo-1501446529957-6226bd447c46?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1489&q=80",
+    image: "../src/assets/slider/slider-02.jpg",
   },
   {
-    image:
-      "https://images.unsplash.com/photo-1483729558449-99ef09a8c325?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    image: "../src/assets/slider/slider-03.jpg",
   },
 ];
 
-module.exports = { sliders };
+const dtimings = [
+  {
+    title: "morning",
+    day: "7:15 AM TO 11:30 AM",
+  },
+  {
+    title: "afternoon",
+    day: "12:30 AM TO 01:00 PM",
+  },
+  {
+    title: "evening",
+    day: "4:00 PM TO 11:30 PM",
+  },
+  {
+    title: "night",
+    day: "8:00 PM TO 08:30 PM",
+  },
+];
+
+const atimings = [
+  {
+    title: "shangar aarti(morning)",
+    day: "7:15 AM",
+  },
+  {
+    title: "sandhya aarti(evening)",
+    day: "7:00 PM",
+  },
+];
+const ttimings = [
+  {
+    title: "afternoon thaal",
+    day: "11:30 AM - 12:30 PM",
+  },
+  {
+    title: "evening thaal",
+    day: "7:30 PM - 8:00 PM",
+  },
+];
+const stimings = [
+  {
+    title: "youth sabha",
+    day: "Friday, 7:30 PM - 10 PM",
+  },
+  {
+    title: "family sabha",
+    day: "Sunday, 10:30 AM - 12:30 PM",
+  },
+];
+module.exports = { sliders, dtimings, atimings, ttimings, stimings };
