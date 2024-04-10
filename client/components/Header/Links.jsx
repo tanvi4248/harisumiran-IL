30d7271b95af3e595f07472b09acd0eb4a61ff00
tanvi4248 <tanvi@GWTN141-10>
2024-04-10 11:36:48 -0500

    add tailwindcss class

--- client/components/Header/Links.jsx
+++ client/components/Header/Links.jsx
@@ -3,12 +3,27 @@ import { Link } from "react-router-dom";
 export default function Links() {
   return (
     <div id="nav-links">
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About us</Link>
-      <Link to="/events">Events</Link>
-      <Link to="/thakorji-darshan">Thakorji Darshan</Link>
-      <Link to="/donate">Donate</Link>
-      <Link to="/contact">Contact</Link>
+      <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 mr-4">
+        Home
+      </Link>
+      <Link to="/about-us" className="block mt-4 lg:inline-block lg:mt-0 mr-4">
+        About us
+      </Link>
+      <Link to="/events" className="block mt-4 lg:inline-block lg:mt-0 mr-4">
+        Events
+      </Link>
+      <Link
+        to="/thakorji-darshan"
+        className="block mt-4 lg:inline-block lg:mt-0 mr-4"
+      >
+        Thakorji Darshan
+      </Link>
+      <Link to="/donate" className="block mt-4 lg:inline-block lg:mt-0 mr-4">
+        Donate
+      </Link>
+      <Link to="/contact" className="block mt-4 lg:inline-block lg:mt-0 mr-4">
+        Contact
+      </Link>
     </div>
   );
 }
