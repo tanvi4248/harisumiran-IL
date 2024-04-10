eef4436a1e70363f9955cf0340a169074fdca31a
tanvi4248 <tanvi@GWTN141-10>
2024-04-10 11:37:20 -0500

    add tailwindcss class

--- client/components/Pages/Home.jsx
+++ client/components/Pages/Home.jsx
@@ -2,11 +2,22 @@ import "react-owl-carousel2/src/owl.carousel.css";
 import "react-owl-carousel2/src/owl.theme.default.css";
 import Slider from "./Slider";
 import YouTubeChannel from "./YouTubeChannel";
-
+import Timing from "./Timing";
+import Atiming from "./Atiming";
+import Ttiming from "./Ttiming";
+import Stiming from "./Stiming";
+import Address from "./Address";
 export default function Home() {
   return (
     <div>
       <Slider></Slider>
+      <Address></Address>
+      <div className="timing-main flex justify-center -mx-3">
+        <Timing></Timing>
+        <Atiming></Atiming>
+        <Ttiming></Ttiming>
+        <Stiming></Stiming>
+      </div>
       <YouTubeChannel channelId="UCizha79cD_LOFJoZbREl0Gg" />
     </div>
   );
