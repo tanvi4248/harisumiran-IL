35d8e50d28e7560ce7ba199af871e6b6e21860a0
tanvi4248 <tanvi@GWTN141-10>
2024-04-10 11:37:46 -0500

    add tailwindcss class

--- client/components/Pages/Timing.jsx
+++ client/components/Pages/Timing.jsx
@@ -1,3 +1,25 @@
+import { useGetAllTimingsQuery } from "../../api/harisumiranApi";
+
 export default function Timing() {
-  return <div>Timing</div>;
+  const { data: timing, error, isLoading } = useGetAllTimingsQuery();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error || !timing) {
+    return <div>Failed to load Timings. Try again later.</div>;
+  }
+  return (
+    <div className="timing w-1/4 bg-secondary mx-3 p-4  text-center">
+      <h2 className="font-bold">Darshan Timing</h2>
+      {timing.map((time) => {
+        return (
+          <div key={time.id}>
+            <p>{time.title}</p>
+            <p>{time.day}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
