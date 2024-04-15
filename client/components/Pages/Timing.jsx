bfbae1ef955d33db426b9f703c274594ba8ff08e
tanvi4248 <tanvi@GWTN141-10>
2024-04-15 16:29:11 -0500

    update

--- client/components/Pages/Timing.jsx
+++ client/components/Pages/Timing.jsx
@@ -10,12 +10,14 @@ export default function Timing() {
     return <div>Failed to load Timings. Try again later.</div>;
   }
   return (
-    <div className="timing w-1/4 bg-secondary mx-3 p-4  text-center">
-      <h2 className="font-bold">Darshan Timing</h2>
+    <div className="timing w-1/4 bg-secondary mx-3 p-4 text-center rounded-lg">
+      <h3 className="p-1 font-semibold bg-title text-white rounded-md">
+        Darshan Timing
+      </h3>
       {timing.map((time) => {
         return (
-          <div key={time.id}>
-            <p>{time.title}</p>
+          <div key={time.id} className="py-3">
+            <p className="capitalize">{time.title}</p>
             <p>{time.day}</p>
           </div>
         );
