d027cd0466ec9b7827bb14232db41c771688a0e5
tanvi4248 <tanvi@GWTN141-10>
2024-04-15 16:27:51 -0500

    add address content

--- client/components/Pages/Address.jsx
+++ client/components/Pages/Address.jsx
@@ -1,9 +1,12 @@
 import temple from "../../src/assets/temple.jpg";
 export default function Address() {
   return (
-    <div className="address-main flex w-1/2 justify-center">
-      <img src={temple} className="w-1/2"></img>
-      <div className="address"> 1670 Checker Rd, Long Grove, IL 60047</div>
+    <div className="address-main">
+      <h1>Address</h1>
+      <div className="address-inner flex w-1/2 justify-center">
+        <img src={temple} className="w-1/2"></img>
+        <div className="address"> 1670 Checker Rd, Long Grove, IL 60047</div>
+      </div>
     </div>
   );
 }
