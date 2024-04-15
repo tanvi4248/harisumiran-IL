77215c2b2b6403a47d5643e689ac260a4f76c758
tanvi4248 <tanvi@GWTN141-10>
2024-04-15 16:39:49 -0500

    update content

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,11 +10,27 @@ export default {
       ...theme("colors"),
       primary: "#fff7ea",
       secondary: "#ebdac5",
+      title: "#6d7f9a",
+    }),
+    textColor: (theme) => ({
+      ...theme("colors"),
+      primary: "#edc386",
     }),
     fontFamily: {
-      body: ["Open Sans"],
+      body: ["Poppins"],
+    },
+    fontWeight: {
+      bold: 700,
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme("fontSize.2xl") },
+        h2: { fontSize: theme("fontSize.xl") },
+        h3: { fontSize: theme("fontSize.lg") },
+      });
+    },
+  ],
 };
